5335ab41e877558728d71c9ec8719d19ae0ef51d
soyoung677 <lsy141@gmail.com>
2017-11-27 22:12:13 -0800

    simplfy source code

--- convert.py
+++ convert.py
@@ -9,27 +9,27 @@ def getInput():
             return int(origin)	
 
 def calHexadecimal(origin):
-    tmpQuotient = 0
-    remainderArray = []
+    quotient = 0
     remainder = 0
+    hexValue = ''
     tmpDividend = origin
     
     while True:
         remainder = changeHex(tmpDividend % 16)
-        remainderArray.append(remainder)
-        tmpQuotient = tmpDividend // 16
+        quotient = tmpDividend // 16
+        hexValue = remainder + hexValue
 
-        if tmpQuotient < 16:	
-            remainderArray.append(changeHex(tmpQuotient))
+        if quotient < 16:	
+            hexValue = changeHex(quotient) + hexValue
             break
         else: 
-            tmpDividend = tmpQuotient
+            tmpDividend = quotient
 
-    return remainderArray
+    return hexValue
 
 def changeHex(value):
     changedVal = 0
-    if value < 10 : changedVal = value
+    if value < 10 : changedVal = str(value)
     if value == 10 : changedVal = 'A'
     if value == 11 : changedVal = 'B'
     if value == 12 : changedVal = 'C'
@@ -38,17 +38,10 @@ def changeHex(value):
     if value == 15 : changedVal = 'F'
     return changedVal
 
-def convertHexStr(remainderArray):
-    strHexValue = ""
-    for i in range(1, len(remainderArray) + 1) :
-        strHexValue += str(remainderArray[len(remainderArray) - i])
-    return strHexValue
-
 def main():
     origin =  getInput()
-    remainderArray = calHexadecimal(origin)
-    strHexValue = convertHexStr(remainderArray)
-    print(origin, ' => ', strHexValue)
+    hexValue = calHexadecimal(origin)
+    print(origin, ' => ', hexValue)
     return
 
 if __name__ == "__main__":
